e8cad28fbec3718a3d65874c9ea554d8d6fcd105
egor <cernogaevegor792@gmail.com>
2026-08-08 18:00:35 +0700

    lexer

--- core/lexer.cpp
+++ core/lexer.cpp
@@ -1,1 +1,48 @@
+/*/   lexer.cpp
+ creator:programost /*/
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <inc/lexer.hpp>
+#include <inc/parser.hpp>
+
+struct VARIABLETYPE var;
+struct SYNTAXCOMAND synx;
+struct SEPSING sep;
+
+string ReadLineFile(string filename) {
+        static ifstream file;
+        static bool opened = false;
+        string line;
+
+        if (!opened) {
+                file.open(filename);
+                if (!file.is_open()) {
+                return string(); 
+                }
+                opened = true;
+        }
+
+        if (getline(file, line)) {
+                return line;  
+        } else {
+                file.close();
+                opened = false;
+                return string(); 
+        }
+}
+
+int tokenize(){
+        string line;
+        while ((line = ReadLineFile("test.yl")) != "") { // Пока значение фиксированное, так что передача имени файла не работает пока что
+                
+        }
+        return 0;
+}
+
+void lexer(){
+        while (1){
+             
+        }
+}
